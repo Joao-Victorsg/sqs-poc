6b02318564607f317fb2a790a89e0934876648c1
João Victor <joaovsg@id.uff.br>
2023-11-26 01:11:31 -0300

    feature: Including JsonNaming in the Mensagem class

--- src/main/java/com/example/sqspoc/adapter/entrypoint/sqs/dto/Mensagem.java
+++ src/main/java/com/example/sqspoc/adapter/entrypoint/sqs/dto/Mensagem.java
@@ -1,11 +1,14 @@
 package com.example.sqspoc.adapter.entrypoint.sqs.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 
 @Builder
 @JsonDeserialize(builder = Mensagem.MensagemBuilder.class)
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record Mensagem (
 
         @JsonProperty
